a1f12f6ba6e74ab75459bca48b81273155e61d25
albertoh <alberto.hernandez@inovatika.cz>
2026-02-25 18:19:29 +0100

    #13

--- src/app/pages/revision/revision-detail/revision-detail.component.ts
+++ src/app/pages/revision/revision-detail/revision-detail.component.ts
@@ -17,11 +17,16 @@ import { Utils } from 'src/app/shared/utils';
 import { OcrEditorComponent } from 'src/app/components/ocr-editor/ocr-editor.component';
 import { ViewerComponent } from 'src/app/components/viewer/viewer.component';
 import { Storage } from 'src/app/shared/constants';
+import { SimpleDialogData } from 'src/app/components/simple-dialog/simple-dialog';
+import { SimpleDialogComponent } from 'src/app/components/simple-dialog/simple-dialog.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-revision-detail',
   standalone: true,
-  imports: [CommonModule, AngularSplitModule, TranslateModule, RouterModule, OcrEditorComponent, ViewerComponent,
+  imports: [CommonModule, AngularSplitModule, TranslateModule, RouterModule,
+    MatDialogModule,
+    OcrEditorComponent, ViewerComponent,
     MatIconModule, MatButtonModule, MatTableModule, MatTooltipModule, HighlightModule],
   templateUrl: './revision-detail.component.html',
   styleUrls: ['./revision-detail.component.scss']
@@ -30,7 +35,7 @@ export class RevisionDetailComponent {
   public version: string = '';
   public pid: string = '';
   public selectedVersion: Version;
-  public altoObject: {pid: string, status: number, version: string};
+  public altoObject: { pid: string, status: number, version: string };
 
   panel0Mode = 'ocr';
   panelMode = 'ocr';
@@ -53,6 +58,7 @@ export class RevisionDetailComponent {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    public dialog: MatDialog,
     private config: AppConfiguration,
     private service: AppService,
     public state: AppState) { }
@@ -87,32 +93,36 @@ export class RevisionDetailComponent {
     localStorage.setItem(Storage.REVIZE_SIZE_BOTTOM_LEFT, String(e.sizes[0]));
     this.viewerWidth = e.sizes[0];
   }
-  
+
   getVersions() {
     this.service.getVersions(this.pid, this.config.login, this.config.instance).subscribe((res: any) => {
+
       this.versions = res.data;
       // find the user's version
       const v = this.versions.find((version: Version) => version.userLogin === this.config.login);
       if (v) {
         this.getVersion(v);
       } else {
-        this.getVersion(this.versions[0]);
+        const shouldContinue = this.processModel(this.versions[0]);
+        if (shouldContinue) {
+          this.getVersion(this.versions[0]);
+        }
       }
     });
   }
 
   getSiblings() {
-      this.previousPid = null;
-      this.nextPid = null;
-      this.service.getSiblings(this.pid, this.config.instance).subscribe((res: any) => {
-        this.previousPid = res.previousPid;
-        this.nextPid = res.nextPid;
-      });
+    this.previousPid = null;
+    this.nextPid = null;
+    this.service.getSiblings(this.pid, this.config.instance).subscribe((res: any) => {
+      this.previousPid = res.previousPid;
+      this.nextPid = res.nextPid;
+    });
   }
 
   getAltoOrig() {
     this.service.getAltoOriginal(this.pid, this.config.login, this.config.instance).subscribe((res: any) => {
-    // this.service.getAltoVersion(this.pid, 'ALTO.0', this.config.login, this.config.instance).subscribe((res: any) => {
+      // this.service.getAltoVersion(this.pid, 'ALTO.0', this.config.login, this.config.instance).subscribe((res: any) => {
       this.altoObject = res;
       this.state.alto0Xml = Utils.prettifyXml(res.data);
       this.state.alto0 = xml2js(this.state.alto0Xml);
@@ -136,6 +146,129 @@ export class RevisionDetailComponent {
     });
   }
 
+  processModel(res: any): boolean {
+
+    // model: OTHER nebo PAGE
+    // versionState: NEW, GENERATED, EDITED, ....
+
+    const model = res.model;
+    const versionState = res.versionState;
+    if (model === 'PAGE') {
+      // Pokud je model=PAGE, tak prosím normálně pokračuj tak, jako to již teď funguje.
+      return true;
+    } else if (model === 'OTHER' && versionState === 'NEW') {
+      // Pokud je model=OTHER a zároveň versionState=NEW tak se musí zobrazit dialogové okno, že jde o více stránek, a jestli chce uživatel spustit hromadné generování stránek.
+      this.multipleGeneration(res);
+      return false;
+    } else if (model === 'OTHER' && versionState === 'GENERATED') {
+      // Pokud je model=OTHER a zároveň versionState=GENERATED tak se musí zobrazit dialogové okno, že jde o více stránek a jestli je chce uživatel nahrát do Krameria.
+      this.multipleUpload(res);
+      return false;
+    } else if (model === 'OTHER' && versionState === 'UPLOADED') {
+      // Pokud je model=OTHER a zároveň versionState=UPLOADED, tak jen zobrazit info, že stránky již byly nahrány do Krameria
+      this.service.showSnackBar('Stránky již byly nahrány do Krameria');
+      this.router.navigate(['/revision']);
+      return false;
+    } else {
+      // Pokud je model=OTHER a zároveň versionState je jiný než NEW, GENERATED a UPLOADED, tak zobrazit info, že daný stav není pro více stránek podporovaný.
+      this.service.showSnackBar('Editace není pro více stránek podporovaná', true);
+      this.router.navigate(['/revision']);
+      return false;
+    }
+
+  }
+
+  multipleGeneration(res: any) {
+    // Pokud je model=OTHER a zároveň versionState=NEW tak se musí zobrazit dialogové okno, že jde o více stránek, a jestli chce uživatel spustit hromadné generování stránek.
+
+    const data: SimpleDialogData = {
+      title: 'Hromadné generování stránek',
+      message: 'Opravdu chcete spustit hromadné generování stránek?',
+      alertClass: 'app-message',
+      btn1: {
+        label: 'Ano',
+        value: 'yes',
+        color: 'warn'
+      },
+      btn2: {
+        label: 'Ne',
+        value: 'no',
+        color: 'default'
+      }
+    };
+    const dialogRef = this.dialog.open(SimpleDialogComponent, {
+      data: data
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'yes') {
+        const instance = this.route.snapshot.queryParams['instance'] ? this.route.snapshot.queryParams['instance'] : this.config.instance;
+        this.generatePERO(this.pid, 'MEDIUM', instance);
+      }
+    });
+
+  }
+
+  generatePERO(pid: string, priority: string, instance: string) {
+    //{"pid":"{{uuidStrana}}","priority":"LOW", "instance":"k7"}
+
+    const data = {
+      pid: pid,
+      priority: priority,
+      instance: instance
+    }
+    this.service.generatePERO(data).subscribe(res => {
+      if (res.errors) {
+        this.service.showSnackBar(res.errors[0], true);
+      } else {
+        this.service.showSnackBar(res.content ? res.content : 'desc.PEROExists');
+      }
+    })
+  }
+
+  multipleUpload(res: any) {
+    // Pokud je model=OTHER a zároveň versionState=GENERATED tak se musí zobrazit dialogové okno, že jde o více stránek a jestli je chce uživatel nahrát do Krameria.
+
+    const data: SimpleDialogData = {
+      title: 'Hromadné nahrání do Krameria',
+      message: 'Záznam má více stránek. Opravdu chcete nahrát do Krameria?',
+      alertClass: 'app-message',
+      btn1: {
+        label: 'Ano',
+        value: 'yes',
+        color: 'warn'
+      },
+      btn2: {
+        label: 'Ne',
+        value: 'no',
+        color: 'default'
+      }
+    };
+    const dialogRef = this.dialog.open(SimpleDialogComponent, {
+      data: data
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'yes') {
+        const data = {
+          pid: this.pid,
+          id: this.state.currentObject.id,
+          login: this.config.login
+        }
+        this.service.uploadKramerius(data).subscribe((res: any) => {
+          if (res.errors) {
+            this.service.showSnackBar(res.errors[0], true);
+          } else {
+            this.service.showSnackBar(res.content);
+          }
+
+        });
+
+
+      }
+    });
+
+  }
+
+
   gotoPrev() {
     this.router.navigate(['/revision', this.previousPid]);
   }
@@ -158,7 +291,7 @@ export class RevisionDetailComponent {
         this.service.showSnackBar('desc.markSuccess');
         this.getVersions();
       }
-      
+
     });
   }
 
@@ -177,7 +310,7 @@ export class RevisionDetailComponent {
         this.getVersions();
       }
     });
-    
+
   }
 
   gotoObject() {
@@ -194,11 +327,11 @@ export class RevisionDetailComponent {
     }
     this.service.uploadKramerius(data).subscribe((res: any) => {
       console.log(res);
-      
+
     });
   }
 
-  setArea(data: {blockIdx: number, lineIdx: number, wordIdx: number}) {
+  setArea(data: { blockIdx: number, lineIdx: number, wordIdx: number }) {
     // const b = this.state.selectedBlocks[0];
     this.state.clearSelection();
     this.state.selectedBlocks = [this.state.printSpace.elements[data.blockIdx]];
@@ -234,7 +367,7 @@ export class RevisionDetailComponent {
         this.state.altoXml = Utils.prettifyXml(res.data);
         this.state.alto = xml2js(this.state.altoXml);
         this.state.printSpace = this.state.setPrintSpace(this.state.alto);
-        
+
         this.getVersions();
       }
     })
